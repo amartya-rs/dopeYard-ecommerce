acaef41d8ae71aadc6c49c193e4ea4294de5f754
amartya-rs <saha.amartya@outlook.com>
2022-03-17 13:53:17 +0530

    refactor: imported & exported components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { HomePageCard } from "./homepage-card/HomePageCard";
+import { TopNav } from "./top-navigation/TopNav";
+import { Footer } from "./footer/Footer";
+
+export { HomePageCard, TopNav, Footer };
