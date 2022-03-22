7188950976852592f865dca9d58d8ff6eaaa8b2b
amartya-rs <saha.amartya@outlook.com>
2022-03-22 16:15:13 +0530

    feat: cart card component

---
+++ src/components/cart-card/CartCard.jsx
@@ -0,0 +1,44 @@
+import { useCart } from "../../context/cart-context";
+
+const CartCard = ({ card }) => {
+   const { state, dispatch } = useCart();
+
+   const addToWishlist = (card) => {
+      dispatch({ type: "ADD_TO_WISHLIST", payload: card });
+   };
+
+   return (
+      <div className="card horizontal">
+         <img className="card-img" src={card.imgUrl} alt="product" />
+         <div className="card-content">
+            <h6 className="font-semibold">{card.title}</h6>
+            <div className="price-wrapper">
+               <h5>₹ 1499</h5>
+               <h6 className="strike-through">{`₹ ${card.price}`}</h6>
+               <h6 className="discount-text">₹50% off </h6>
+            </div>
+            <div className="cart-quantity">
+               <label htmlFor="quantity">Quantity: </label>
+               <button className="font-medium">+</button>
+               <input
+                  value="1"
+                  className="quantity-input h6 font-normal"
+                  name="quantity"
+               />
+               <button className="font-medium">-</button>
+            </div>
+            <button className="button secondary">
+               <span>REMOVE FROM CART</span>
+            </button>
+            <button
+               onClick={() => addToWishlist(card)}
+               className="button outline-secondary"
+            >
+               WISHLIST
+            </button>
+         </div>
+      </div>
+   );
+};
+
+export { CartCard };
