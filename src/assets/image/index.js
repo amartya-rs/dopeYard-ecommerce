bb1042da81656d8e40e00ad874660265c6d17a7c
amartya-rs <saha.amartya@outlook.com>
2022-03-26 11:35:45 +0530

    refactor: added new images

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -4,13 +4,17 @@ import em_hoodie_1 from "./em_hoodie_1.webp";
 import em_hoodie_2 from "./em_hoodie_2.jpg";
 import em_tshirt_1 from "./em_tshirt_1.png";
 import em_tshirt_2 from "./em_tshirt_2.webp";
+import em_tshirt_3 from "./em_tshirt_3.webp";
 import em_lsleeve_1 from "./em_lsleeve_1.webp";
 import em_lsleeve_2 from "./em_lsleeve_2.webp";
 import em_lsleeve_3 from "./em_lsleeve_3.jpg";
+import em_lsleeve_4 from "./em_lsleeve_4.webp";
 import sold_out_img from "./sold_out_img.png";
 import em_hat_3 from "./em_hat_3.webp";
-import em_lsleeve_4 from "./em_lsleeve_4.webp";
-import em_tshirt_3 from "./em_tshirt_3.webp";
+import category_cap from "./category_cap.webp";
+import category_hoodie from "./category_hoodie.webp";
+import category_tshirt from "./category_tshirt.webp";
+import category_lSleeve from "./category_lSleeve.webp";
 
 export {
    em_hat_1,
@@ -26,4 +30,8 @@ export {
    em_lsleeve_3,
    em_lsleeve_4,
    sold_out_img,
+   category_cap,
+   category_tshirt,
+   category_hoodie,
+   category_lSleeve,
 };
