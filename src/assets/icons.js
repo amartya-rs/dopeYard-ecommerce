04f56c7eb790322d21fc1fd98d776067f65fd27c
amartya-rs <saha.amartya@outlook.com>
2022-06-11 14:39:38 +0530

    added icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -112,4 +112,55 @@ const LogoutIcon = (props) => {
    );
 };
 
-export { HeartIcon, CartIcon, UserIcon, CrossIcon, Star, LogoutIcon };
+const Edit = (props) => {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="rgb(145, 55, 135)"
+         strokeWidth="2"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+         className="feather feather-edit"
+         {...props}
+      >
+         <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+         <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+      </svg>
+   );
+};
+
+const Delete = (props) => {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         width="24"
+         height="24"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="rgb(145, 55, 135)"
+         strokeWidth="2"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+         className="feather feather-trash"
+         {...props}
+      >
+         <polyline points="3 6 5 6 21 6" />
+         <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      </svg>
+   );
+};
+
+export {
+   HeartIcon,
+   CartIcon,
+   UserIcon,
+   CrossIcon,
+   Star,
+   LogoutIcon,
+   Edit,
+   Delete,
+};
